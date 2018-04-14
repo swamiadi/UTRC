4f9281dea822885fc74e15f43024de4783b1dc21
Aditya Swami <adityaswami89@gmail.com>
2018-04-14 19:50:08 +0530

    Adding Spinner
    Adding Spinner for user to wait

--- SocialNetwork/SocialNetwork/Program.cs
+++ SocialNetwork/SocialNetwork/Program.cs
@@ -15,15 +15,28 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SocialNetwork
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.Write("Please enter the values of A" + Environment.NewLine);
+            var personA = Console.ReadLine()?.Trim();
+            Console.Write("Please enter the values of B" + Environment.NewLine);
+            var personB = Console.ReadLine()?.Trim();
+
             if (File.Exists(@"Model.txt"))
             {
                 var modelfile = System.IO.File.ReadAllText(@"Model.txt");
                 var graph = StringToObject(modelfile) as List<GraphNode<string>>;
-                FindShortestPath(graph);
+                Console.Write("Total number of people in the social network : {0}",graph?.Count);
+                FindShortestPath(graph, personA, personB);
 
                 Console.ReadLine();
             }
@@ -35,7 +48,7 @@ namespace SocialNetwork
 
                 int loopcount = 0;
                 var exceptions = new ConcurrentQueue<Exception>();
-                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var watch = Stopwatch.StartNew();
 
                 var tokenSource = new CancellationTokenSource();
                 CancellationToken ct = tokenSource.Token;
@@ -46,7 +59,7 @@ namespace SocialNetwork
                     GraphNode<string> node = new GraphNode<string>(item);
                     graphnodes.Add(node);
                 }
-
+                Console.Write("Total number of people in the social network : {0}", graphnodes.Count);
                 Task task = Task.Factory.StartNew(delegate
                 {
                     // Were we already canceled?
@@ -60,7 +73,6 @@ namespace SocialNetwork
                     {
                         try
                         {
-
 
                             Interlocked.Increment(ref loopcount);
                             Console.WriteLine(loopcount);
@@ -82,25 +94,32 @@ namespace SocialNetwork
 
                 }, tokenSource.Token); // Pass same token to StartNew.
 
-                task.ContinueWith(antecendent => SignalCompletion(watch, graphnodes), ct);
+                task.ContinueWith(antecendent => SignalCompletion(watch, graphnodes,personA,personB), ct);
 
             }
             #endregion
             Console.ReadLine();
         }
 
-        private static void SignalCompletion(Stopwatch watch, List<GraphNode<string>> graph)
+        private static void SignalCompletion(Stopwatch watch, List<GraphNode<string>> graph, string PersonA, string PersonB)
         {
-            var stringvalue = ObjectToString(graph);
-            System.IO.File.WriteAllText(@"Model.txt", stringvalue);
+            try
+            {
+                var stringvalue = ObjectToString(graph);
+                System.IO.File.WriteAllText(@"Model.txt", stringvalue);
 
-            watch.Stop();
-            var elapsedms = watch.ElapsedMilliseconds;
-            Console.WriteLine(TimeSpan.FromMilliseconds(elapsedms).TotalMinutes);
+                watch.Stop();
+                var elapsedms = watch.ElapsedMilliseconds;
+                Console.WriteLine(TimeSpan.FromMilliseconds(elapsedms).TotalMinutes);
 
-            FindShortestPath(graph);
+                FindShortestPath(graph, PersonA, PersonB);
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         /// <summary>
@@ -109,25 +128,34 @@ namespace SocialNetwork
         /// <param name="graph"></param>
         /// <param name="from"></param>
         /// <param name="to"></param>
-        private static void FindShortestPath(List<GraphNode<string>> graph, string from = "", string to = "")
+        private static void FindShortestPath(List<GraphNode<string>> graph, string from , string to )
         {
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            var dijkstra = new Dijkstra<string>(graph);
-            var path = dijkstra.FindShortestPathBetween(graph.FirstOrDefault(x => x.Value == "STACEY_STRIMPLE"), graph.FirstOrDefault(x => x.Value == "RICH_OMLI"));
-            watch.Stop();
-            var elapsedms = watch.ElapsedMilliseconds;
-            Console.WriteLine(TimeSpan.FromMilliseconds(elapsedms).TotalMinutes);
-            foreach (var item in path)
+            try
             {
-                Console.WriteLine(item.Value + Environment.NewLine);
-            }
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var dijkstra = new Dijkstra<string>(graph);
+                var path = dijkstra.FindShortestPathBetween(graph.FirstOrDefault(x => x.Value == from), graph.FirstOrDefault(x => x.Value == to));
+                watch.Stop();
+                var elapsedms = watch.ElapsedMilliseconds;
+                Console.WriteLine("Total Time required to search : {0}",TimeSpan.FromMilliseconds(elapsedms).TotalMinutes);
+                Console.WriteLine(Environment.NewLine + "Distance between {0} and {1} is {2}",from,to,path.Count - 1);
+                foreach (var item in path)
+                {
+                    Console.WriteLine(item.Value + Environment.NewLine);
+                }
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
 
 
-        public static string ObjectToString(object obj)
+        private static string ObjectToString(object obj)
         {
             using (MemoryStream ms = new MemoryStream())
             {
@@ -136,7 +164,7 @@ namespace SocialNetwork
             }
         }
 
-        public static object StringToObject(string base64String)
+        private static object StringToObject(string base64String)
         {
             byte[] bytes = Convert.FromBase64String(base64String);
             using (MemoryStream ms = new MemoryStream(bytes, 0, bytes.Length))
